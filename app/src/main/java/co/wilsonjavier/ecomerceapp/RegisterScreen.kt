72f58e3c9a534405e66bba671fd4022da5a7e859
wilson gomez <wgomez415@unab.edu.co>
2025-04-11 02:12:10 -0500

    DISEÑO REGISTER SCREEN

--- app/src/main/java/co/wilsonjavier/ecomerceapp/RegisterScreen.kt
+++ app/src/main/java/co/wilsonjavier/ecomerceapp/RegisterScreen.kt
@@ -13,47 +13,40 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun LoginScreen() {
+fun RegisterScreen(){
     Scaffold { innerPadding ->
-        // Contenedor principal
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-                .padding(horizontal = 30.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            // Imagen de logo
-            Image(
-                painter = painterResource(id = R.drawable.logo), // Asegúrate de tener la imagen en la carpeta drawable
-                contentDescription = "Logo Unab",
-                modifier = Modifier.size(150.dp)
+        Column (modifier = Modifier.padding(innerPadding).fillMaxSize().padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Image(imageVector = Icons.Default.Person,
+                contentDescription = null,
+                modifier = Modifier.size(150.dp),
+                colorFilter = ColorFilter.tint(Color(0xFFF9900))
             )
 
             Spacer(modifier = Modifier.height(24.dp))
 
             // Título
             Text(
-                text = "Iniciar Sesión",
+                text = "REGISTRO",
                 fontSize = 24.sp,
                 color = Color(0xFFF9988),
                 fontWeight = FontWeight.Bold,
@@ -79,6 +72,7 @@ fun LoginScreen() {
                 },
                 shape = RoundedCornerShape(12.dp)
             )
+            Spacer(modifier = Modifier.height(24.dp))
             OutlinedTextField(
                 value = "",
                 onValueChange = {},
@@ -96,6 +90,42 @@ fun LoginScreen() {
                 shape = RoundedCornerShape(12.dp)
             )
             Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Email",
+                        tint = Color(0xFFF9900)
+                    )
+                },
+                label = {
+                    Text(text = "Confirmar Contraseña")
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Campo de texto para correo
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email",
+                        tint = Color(0xFFF9900)
+                    )
+                },
+                label = {
+                    Text(text = "Nombre Completo")
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
 
             Button(onClick = {},modifier = Modifier
                 .fillMaxWidth()
@@ -103,20 +133,15 @@ fun LoginScreen() {
                 shape = RoundedCornerShape(12.dp),
                 colors = ButtonDefaults.buttonColors()
             ) {
-                Text("Iniciar sesion")
+                Text("Registrarse")
             }
 
-            Spacer(modifier = Modifier.height(24.dp))
-
-            TextButton(onClick = {}) {
-                Text(text = "No tienes cuenta? ,REGISTRATE")
-            }
         }
     }
 }
 
-@Composable
 @Preview
-fun LoginScreenPreview(){
-    LoginScreen()
+@Composable
+fun RegisterScreenPreview(){
+    RegisterScreen()
 }
